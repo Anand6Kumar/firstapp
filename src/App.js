a4862929487fcf7c84a4c8ff291da21ca9f2c346
Anand6Kumar <anand_kumar6@persistent.com>
2022-09-12 14:47:34 +0530

    json-server addition

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
 
       <body>
 
-        <nav class="navbar">
-          <div class="nav">
+        <nav className="navbar">
+          <div className="nav">
             <img src="https://w7.pngwing.com/pngs/11/552/png-transparent-online-shopping-mobile-phone-shopping-phone-icon-coffee-shop-shopping-bags-trolleys-thumbnail.png"
-              class="brand-logo" alt="Shopping Cart" />
-            <div class="nav-items">
-              <div class="search">
-                <input type="text" class="search-box" placeholder="search brand, product" />
-                <button class="search-btn">search</button>
+              className="brand-logo" alt="Shopping Cart" />
+            <div className="nav-items">
+              <div className="search">
+                <input type="text" className="search-box" placeholder="search brand, product" />
+                <button className="search-btn">search</button>
               </div>
               <a href="#"><img
                 src="https://w1.pngwing.com/pngs/743/500/png-transparent-circle-silhouette-logo-user-user-profile-green-facial-expression-nose-cartoon-thumbnail.png"
@@ -24,35 +24,14 @@ function App() {
                 alt="cart" /></a>
             </div>
           </div>
-          <ul class="links-container">
-            <li class="link-item"><a href="#" class="link">home</a></li>
-            <li class="link-item"><a href="#" class="link">women</a></li>
-            <li class="link-item"><a href="#" class="link">men</a></li>
-            <li class="link-item"><a href="#" class="link">accessories</a></li>
+          <ul className="links-container">
+            <li className="link-item"><a href="#" className="link">home</a></li>
+            <li className="link-item"><a href="#" className="link">women</a></li>
+            <li className="link-item"><a href="#" className="link">men</a></li>
+            <li className="link-item"><a href="#" className="link">accessories</a></li>
           </ul>
         </nav>
         <div id="productData"> </div>
-
-        {/* <!-- <div class="test">
-        <div class="card">
-            <img src="https://w7.pngwing.com/pngs/462/672/png-transparent-feature-phone-smartphone-cellular-network-phone-gadget-phone-icon-mobile-phone-thumbnail.png"
-                alt="Denim Jeans" style="width:100%">
-            <h1>Samsung Phone</h1>
-            <p class="price">$350</p>
-            <p>32GB Slim Samsung mobile Never Settle Down </p>
-            <p><button>Add to Cart</button></p>
-        </div> -->
-
-    <!-- <div class="card">
-            <img
-                src="https://w7.pngwing.com/pngs/723/143/png-transparent-shoe-nike-free-air-force-nike-shoes-image-file-formats-fashion-outdoor-shoe-thumbnail.png">
-            <h1>Nike Shoes</h1>
-            <p class="price">$85</p>
-            <p>Comfort Nike Shoes for Casual Wear </p>
-            <p><button>Add to Cart</button></p>
-        </div> --> */}
-
-
 
       </body>
 
